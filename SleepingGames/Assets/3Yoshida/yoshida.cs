8019732a2bbb4d3f60aaaf74cb64b955008d926c
REN <reny58817@gmail.com>
2024-10-21 15:43:35 +0900

    no message

---
+++ SleepingGames/Assets/3Yoshida/yoshida.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yoshida : MonoBehaviour
+{
+
+	Rigidbody2D rigid2d;
+	Vector2 startPos;
+	private float speed;
+
+
+	void Start()
+	{
+		this.rigid2d = GetComponent<Rigidbody2D>();
+		this.speed = 100;
+
+	}
+
+	void Update()
+	{
+
+		// マウスの動きと反対方向に発射される
+		if (Input.GetMouseButtonDown(0))
+		{
+			this.startPos = Input.mousePosition;
+		}
+		else if (Input.GetMouseButtonUp(0))
+		{
+			Vector2 endPos = Input.mousePosition;
+			Vector2 startDirection = -1 * (endPos - startPos).normalized;
+			this.rigid2d.AddForce(startDirection * 500);
+		}
+
+		// テスト用：スペースキー押下で停止
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			this.rigid2d.velocity *= 0;
+		}
+
+
+		// Spaceキーを離すと動き出す
+		//if(Input.GetKeyUp(KeyCode.Space)) {
+		//
+		//	this.speed = 100.0f;
+		//	this.rigid2d.AddForce(transform.up * speed);
+		//}
+
+	}
+
+	void FixedUpdate()
+	{
+
+		this.rigid2d.velocity *= 0.995f;
+
+	}
+
+}
